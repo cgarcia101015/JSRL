dcb8e1d6677247de9bd0f8fe86dead50760367b0
Carlos M Garcia <cgarcia101015@gmail.com>
2022-01-09 22:48:40 -0500

    else

--- code/app.js
+++ code/app.js
@@ -1,1 +1,13 @@
-alert("It's working!");
+let rating = "A";
+
+if (rating === 3) {
+    console.log('YOU ARE A SUPERSTAR!');
+}
+else if (rating === 2) {
+    console.log('MEETS EXPECTATIONS');
+} else if(rating ===1) {
+    console.log('NEEDS IMPROVEMENT');
+}
+else {
+    console.log('INVALID RATING');
+}
